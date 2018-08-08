28bb6f18808b84ef520d1b3445b4fb9198ddb617
Tom Willmott <tom.willmott@hotmail.com>
2018-08-08 20:15:27 +0100

    Adding book list container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The Good Bits' },
+        { title: 'Stormbreaker' },
+        { title: 'Jack Reacher' },
+        { title: 'Eloquent Ruby' }
+    ]
+}
